05f0792fdfae740dce38329eca71caf8409d6b78
inquis1t0r <piotr.kazimierski92@wp.pl>
2020-07-04 22:52:57 +0200

    MessagesCommand added

---
+++ Watchman.DomainModel/Messages/Commands/MessagesCommand.cs
@@ -0,0 +1,15 @@
+﻿using Devscord.DiscordFramework.Framework.Commands.PropertyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.DomainModel.Messages.Commands
+{
+    class MessagesCommand
+    {
+        [UserMention]
+        public string User { get; set; }
+        [ChannelMention]
+        public string Channel { get; set; }
+    }
+}
