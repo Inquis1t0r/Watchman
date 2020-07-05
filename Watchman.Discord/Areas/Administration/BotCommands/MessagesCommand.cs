023541ae0dcf434c144be0585b19527e923c27a2
inquis1t0r <piotr.kazimierski92@wp.pl>
2020-07-05 20:36:25 +0200

    MessagesCommand added

---
+++ Watchman.Discord/Areas/Administration/BotCommands/MessagesCommand.cs
@@ -0,0 +1,16 @@
+﻿using Devscord.DiscordFramework.Framework.Commands;
+using Devscord.DiscordFramework.Framework.Commands.PropertyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchman.Discord.Areas.Administration.BotCommands
+{
+    class MessagesCommand : IBotCommand
+    {
+        [UserMention]
+        public ulong User { get; set; }
+        [ChannelMention]
+        public ulong Channel { get; set; }
+    }
+}
