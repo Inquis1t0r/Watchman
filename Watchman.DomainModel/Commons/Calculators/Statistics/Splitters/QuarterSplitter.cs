c5124d98b027cd8c2919992f95e0ff0d42b1f787
inquis1t0r <piotr.kazimierski92@wp.pl>
2020-07-15 18:45:59 +0200

    QuarterSplitter added

---
+++ Watchman.DomainModel/Commons/Calculators/Statistics/Splitters/QuarterSplitter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Watchman.Common.Models;
+
+namespace Watchman.DomainModel.Commons.Calculators.Statistics.Splitters
+{
+    public class QuarterSplitter : ISplitter
+    {
+        public IEnumerable<KeyValuePair<TimeRange, IEnumerable<T>>> Split<T>(IEnumerable<T> collection) where T : ISplittable
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
